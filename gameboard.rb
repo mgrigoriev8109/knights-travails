08a7ffddcf3e0607d56930078162bbf2e0038035
Mikhail <mikhailgrigoriev92@gmail.com>
2021-10-05 22:08:00 -0400

    Create GameBoard class

---
+++ gameboard.rb
@@ -0,0 +1,23 @@
+class GameBoard
+  8
+  attr_accessor :gameboard
+
+  def initialize(gameboard = [])
+    @gameboard = gameboard
+  end
+
+  def create_gameboard
+    @gameboard = Array.new(8, "x"){Array.new(8, "x")}
+  end
+
+  def display_gameboard
+    @gameboard.each do |space|
+      puts space.join(" ")
+    end
+end
+
+end
+
+new_gameboard = GameBoard.new
+new_gameboard.create_gameboard
+new_gameboard.display_gameboard
