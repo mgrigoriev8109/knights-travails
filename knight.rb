65b1155f2e0beff1fa0e2f8b14d015478d608025
Mikhail <mikhailgrigoriev92@gmail.com>
2021-10-05 22:24:09 -0400

    Create pseudocode for #generate_possible_moves

--- knight.rb
+++ knight.rb
@@ -2,19 +2,27 @@ class Knight
   attr_accessor :moves
 
   def initialize
-    @moves = []
+    @possible_moves = generate_possible_moves
   end
 
-  def knight_moves(start, end)
-    if current_node.location == end
+  def knight_moves(start, destination)
+    if current_node.location == destination
       return least moves_traveled
     else
       moves_traveled += 1
-      @moves.each do 
-        moves_traveled = knight_moves(current_node, end)
+      @possible_moves.each do 
+        moves_traveled = knight_moves(current_node, destination)
       end
     end
     moves_traveled
+  end
+
+  def generate_possible_moves(location)
+    function that takes a point on a 2d array
+    and creates an array of possible end-points
+    given that a knight can move two one way one another way
+
+    todo this
   end
 
 end
