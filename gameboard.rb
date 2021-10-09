09626646d67efaeea7d95b253404ddb7ef2f4f5b
Mikhail <mikhailgrigoriev92@gmail.com>
2021-10-08 20:26:11 -0400

    Working on #update_gameboard and #display_gameboard

--- gameboard.rb
+++ gameboard.rb
@@ -13,16 +13,26 @@ class GameBoard
     @gameboard = [Array.new(8, "x"){Array.new(8, "x")}]
   end
 
-  def update_gameboard_with_path(array)
+  def update_gameboard(array)
     array.each do |node_on_path|
-      @gameboard(node_on_path) = k
+      p node_on_path
+      @gameboard[node_on_path] = 'k'
+    end
+  end
+
+  def display_gameboard
+    @gameboard.each do |row|
+     p row
     end
   end
 
 end
-
+first_knight = Knight.new
 new_gameboard = GameBoard.new
 new_gameboard.create_gameboard
-new_gameboard.update_gameboard(knight_moves([7,7],[0,0]))
+new_gameboard.gameboard[7,7] = 's' 
+new_gameboard.gameboard[0,0] = 'f' 
 new_gameboard.display_gameboard
-p new_gameboard.gameboard
+new_gameboard.update_gameboard(first_knight.knight_moves([7,7],[0,0]))
+puts "s marks the start, f marks the finish, and k marks the shortest path moves inbetween"
+new_gameboard.display_gameboard
