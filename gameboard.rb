d2ac68b24c3ef7fd2d49fc56ff37ccfc76f3f524
Mikhail <mikhailgrigoriev92@gmail.com>
2021-10-08 20:15:08 -0400

    Brainstorm pseudocode for dislaying chess gameboard

--- gameboard.rb
+++ gameboard.rb
@@ -1,3 +1,6 @@
+require_relative "knight"
+require_relative "node"
+
 class GameBoard
 
   attr_accessor :gameboard
@@ -10,9 +13,9 @@ class GameBoard
     @gameboard = [Array.new(8, "x"){Array.new(8, "x")}]
   end
 
-  def display_gameboard
-    @gameboard.each do |space|
-      puts space.join(" ")
+  def update_gameboard_with_path(array)
+    array.each do |node_on_path|
+      @gameboard(node_on_path) = k
     end
   end
 
@@ -20,5 +23,6 @@ end
 
 new_gameboard = GameBoard.new
 new_gameboard.create_gameboard
+new_gameboard.update_gameboard(knight_moves([7,7],[0,0]))
 new_gameboard.display_gameboard
-p new_gameboard.gameboard[0][0]
+p new_gameboard.gameboard
